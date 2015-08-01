ff42339ce5d7f5baa054a17c63e29026853de0ea
David Vuong <david.vuong256@gmail.com>
2015-08-01 12:21:49 +1000

    feat(examples): add examples (skeleton)

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * ngNavigation/examples/app.js
+ *
+ * Copyright (c) 2015 David Vuong <david.vuong256@gmail.com>
+ * Licensed MIT
+ */
+var express = require('express');
+var serveStatic = require('serve-static');
+
+var app = express();
+
+app.use(serveStatic(__dirname + '/examples'));
+app.use('/js', serveStatic(__dirname + '/js'));
+app.use('/js', serveStatic(__dirname + '/node_modules'));
+app.listen(3000);
+
+console.log('Serving examples/ on port 3000...');
---
+++ examples/main.js
@@ -0,0 +1,60 @@
+/**
+ * ngNavigation/examples/main.js
+ *
+ * Copyright (c) 2015 David Vuong <david.vuong256@gmail.com>
+ * Licensed MIT
+ */
+(function () {
+    'use strict';
+
+    var app = angular.module('ngNavigationTest', [
+        'ngRoute',
+        'ngNavigation'
+    ]);
+
+    function config($routeProvider) {
+        // NOTE: You don't have to use ngRoute, ui-router works too.
+        $routeProvider.when('/example1', {
+            templateUrl: 'templates/route1.tpl.html',
+            controller: 'RouteExampleCtrl1'
+        }).
+        when('/example2', {
+            templateUrl: 'templates/route2.tpl.html',
+            controller: 'RouteExampleCtrl2'
+        }).
+        when('/example3', {
+            templateUrl: 'templates/route3.tpl.html',
+            controller: 'RouteExampleCtrl3'
+        }).
+        when('/example4', {
+            templateUrl: 'templates/route4.tpl.html',
+            controller: 'RouteExampleCtrl4'
+        }).otherwise({
+            redirectTo: '/example1'
+        });
+    }
+
+    function run(Navigation) {
+        Navigation.init();
+    }
+
+    app
+        .config(config)
+        .run(run)
+        .controller('AppCtrl', function () { });
+
+    /* Example route controllers. */
+    app.controller('RouteExampleCtrl1', function (Navigation) {
+
+    });
+    app.controller('RouteExampleCtrl2', function (Navigation) {
+
+    });
+    app.controller('RouteExampleCtrl3', function (Navigation) {
+
+    });
+    app.controller('RouteExampleCtrl4', function (Navigation) {
+
+    });
+
+})();

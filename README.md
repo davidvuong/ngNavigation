90db3b878c5be8c9f467ae020e39812c56823aba
David Vuong <david.vuong256@gmail.com>
2015-08-09 14:51:10 +1000

    docs(readme+ngNavigation): documented the api

--- README.md
+++ README.md
@@ -35,7 +35,99 @@ app.controller('AppCtrl', function ($scope, Navigation) {
 
 ### Documentation
 
-TODO!
+ngNavigation provides a small API to control your application's navigation. The following are the public methods available.
+
+#### `.init(options)`
+
+`.init` Initializes the ngNavigation service, registering a listener to route changes. This should be called before you make other calls to ngNavigation. These route changes can be with through `href|ng-href` links in the view, `$location.path` calls via Angular, or `routeTo` calls through the `ngNavigation` API.
+
+The `.init` method takes an optional argument, `options` in the form:
+
+```js
+{ appendSlash: false, stripSlash: false }
+```
+
+By default, both are set to `false`. These properties are mutually exclusive, meaning only one can be set to true at any single time. They're also optional.
+
+#### `.routeTo(url, options)`
+
+`.routeTo` allows clients to route to a new page in your app. The method takes in 2 arguments. The first is the url to route to and the second is an object in the form:
+
+```js
+{ params: {}, clearStack: false };
+```
+
+For example:
+
+```js
+$scope.routeToHome = function () {
+    ngNavigation.routeTo('/home');
+};
+```
+
+Sometimes you need additional query params when routing, you can do that in two ways:
+
+```js
+// The first method is to just have it in the url.
+ngNavigation.routeTo('/accounts?tab=payments')
+
+// The other method is to pass it into `params`.
+var options = { params: { tab: 'payments' } };
+ngNavigation.routeTo('/accounts', options);
+
+// NOTE: `options.params` will override the params in the url.
+var options = { params: { tab: 'payments' } };
+ngNavigation.routeTo('/accounts?tab=details', options);
+```
+
+`.routeTo` can also be called with `options = { clearStack: true }`. This is useful when you want to clear the route history before or after routing. For example, you have a side-menu that's present throughout the app. The side-menu is the root of your navigation. It does not make much sense to be able to go back after re-routing via the root.
+
+#### `.back(fallbackRoute)`
+
+`.back` calls `$window.history.back()` when a previous route is available, popping from the route stack. When no routes are available, no operations are made. `.back` also takes in an optional argument, `fallbackRoute` in the form:
+
+```js
+{ url: '...', params: {} };
+```
+
+Having a `fallbackRoute` ensures that if the route stack is empty, the call to `.back` will fallback to the provided `fallbackRoute`.
+
+The `fallbackRoute` could be useful when you have external deep links to your app (e.g. an email notification). When a user clicks on the deep link (e.g. `/app/settings/notifications`), the route stack is empty. This means if you have a navigation bar with a back button that hooks onto `ngNavigation.back`, clicking that button won't take you back because the stack is empty. Having a `fallbackUrl` will ensure that navigation after deep links work. For example:
+
+```js
+// navigation.js (navbar controller).
+$scope.navigateBack = function () {
+    ngNavigation.back({ url: '/settings' });
+};
+```
+
+#### `.peakRouteStack()`
+
+Peaks at the top of the route stack, returning the most previous route.
+
+#### `.pushToRouteStack(url, options)`
+
+Pushes to the route stack without routing. The `options` argument shares the same properties as those defined in `routeTo` with the exception of `clearStack`.
+
+#### `.isBackRouteAvailable()`
+
+Returns whether or not the route stack is empty.
+
+#### `.clearRouteStack()`
+
+Clears the route stack of all existing routes.
+
+#### `.getRouteStack()`
+
+Returns a reference to the route stack.
+
+#### `.getDefaultOptions()`
+
+Returns the options object. These default options can be updated through the `.init` call.
+
+---
+
+*Please take a look at `ngNavigation/examples/` or read the source for more examples. ngNavigation is an incredibly simple service without much code at all.*
 
 ### Contributions
 
--- js/ngNavigation.js
+++ js/ngNavigation.js
@@ -185,14 +185,14 @@
             fallbackRoute = fallbackRoute || {};
 
             var isBackRouteAvailable = self.isBackRouteAvailable();
-            var isfallbackRouteAvailable = !angular.equals(fallbackRoute, {});
+            var isFallbackRouteAvailable = !angular.equals(fallbackRoute, {});
 
             // No routes in the routeStack & no fallback provided.
-            if (!isBackRouteAvailable && !isfallbackRouteAvailable) {
+            if (!isBackRouteAvailable && !isFallbackRouteAvailable) {
                 return;
             }
             // No routes in the routeStack however fallback provided.
-            if (!isBackRouteAvailable && isfallbackRouteAvailable) {
+            if (!isBackRouteAvailable && isFallbackRouteAvailable) {
                 return self._p._route(fallbackRoute.url, fallbackRoute.params || {});
             }
             // There are routes in the routeStack.
@@ -219,42 +219,10 @@
         };
 
         /**
-         * ngNavigation Plan & Initial Documentation
-         *
-         * Description
-         * ===========
-         * ngNavigation's goal is to provide a simple interface to navigate to
-         * and from routes. It should support the ability to make explicit calls
-         * to JavaScript functions as well as be able to listen to route changes.
-         *
-         * API
-         * ===
-         * `.routeTo(url, options)`
-         *  Routes the user to `url`, updating the route stack with the current
-         *  route (prior to routing). See "Edge Cases" for `options`.
-         *
-         * `.init(options)`
-         *  Initializes the ngNavigation before/after route changes so ngNavigation
-         *  is able to easily push/pop entries from the navigation stack.
-         *  See "Edge Cases" for `options`.
-         *
-         * `back(url)`
-         *  Goes back to the previous route in the route stack. If a url is provided
-         *  then the route stack is cleared (`url` might not be needed).
-         *
-         * `clearRouteStack()`
-         *  Clears the route stack, `_routeStack.length = 0`
-         *
-         * `isBackRouteAvailable()`
-         *  Checks if the route stack is empty.
-         *
-         * `pushToRouteStack(url, options)`
-         *  Pushes to the route stack without routing.
-         *
          * Edge Cases
          * ==========
          *
-         * 1. Deep route stack (10+ back routes) (partially done, needs testing)
+         * 1. Deep route stack (10+ back routes)
          *  There are a couple things we could do to reduce having a large stack. They include:
          *      - If the `url` in `routeTo` is the same as `_routeStack.peak` then
          *      pop from the `_routeStack` instead of pushing onto it (causing A, B, A, B ...).
@@ -267,40 +235,40 @@
          *      - The last solution, a bit more explicit, when the client knows the next `routeTo`
          *      does not care about the back route then `routeTo(url, { clearStack: true })`.
          *
-         * 2. Race conditions with watchers and explicit stack push (done)
+         * 2. Race conditions with watchers and explicit stack push
          *  There might be a case where we're updating the route stack, at the same time we get
          *  signaled that a route is about to happen.
          *      - Until we're experiencing serious issues, have a simple flag that gets set
          *      whenever we're updating the route. If the flag is set, every other stack updates
          *      gets dropped.
          *
-         * 3. Routes with hashes/options etc. (done)
+         * 3. Routes with hashes/options etc.
          *  AngularJS's $location.path isn't friendly when we have params in the url. For example
          *  `/accounts?tab=payments`.
          *      - The easiest solution is to force `routeTo` to allow a search option in the
          *      `options` object: `options = { search: { tab: 'payments' } }`
          *      - The alternative is we expect it to be in the `url` and we parse it out ourselves.
          *
-         * 4. Empty route stack but `back` is called (done)
+         * 4. Empty route stack but `back` is called
          *  So ideally we'd use the browser's native `$window.location.back()` so when going back
          *  the browser automatically scrolls to where it left off however it may result in
          *  the browser taking us back, prior to when our site was hit.
          *      - The solution is to just not allow `back` to do anything when the stack is empty
          *      See `rootUrl` for possible additions.
          *
-         * 5. Routing to `urlB` where the current `urlA` isn't the back route (not needed)
+         * 5. Routing to `urlB` where the current `urlA` isn't the back route
          *  So, I'm at `urlA` and I route to `urlB` but I don't want to go back to `urlA`
          *  when I call `back()`, instead I want to go to `urlC`.
          *      - In the `routeTo`'s option object, include a `{ backUrl: '/blah' }`.
          *      Just have to make sure that `backUrl` also gets correctly parsed
          *
-         * 6. The provided `url` or `backUrl` is not a valid url (done)
+         * 6. The provided `url` or `backUrl` is not a valid url
          *  It doesn't really matter, the browser won't do anything silly. The only problem is
          *  when the options are invalid `?x==&&&==b`.
          *      - Throw an exception that the provided `search` in url or options is invalid
          *      and could not be parsed.
          *
-         * 7. Sporadically changing urls (done)
+         * 7. Sporadically changing urls
          *  Let's say you hit `/route-one` and in the controller, it will always either switch
          *  to `/route-two` or `/route-three`. The back route will always be `/route-one`.
          *  Going back to `/route-one` will then route me to `/route-{two,three}` causing
@@ -310,7 +278,7 @@
          *      - A temporary solution is to do `routeTo(url, { clearStack: true })` so it
          *      avoids the growing stack. This raises another issue though... (8)
          *
-         * 8. Not at the root route and the route stack is empty (done, option 2)
+         * 8. Not at the root route and the route stack is empty
          *  Let's say I've loaded up the page and my root route is `#/home` but I've loaded up
          *  `#/accounts` first. When I try to go back to `#/home`, the `back()` won't work.
          *      - One simple solution is in the `init` call, let clients specify a rootUrl.
@@ -320,7 +288,7 @@
          *      is hit and the stack is empty, push a back route. This means 2 additional
          *      methods need to be exposed, `isBackRouteAvailable`, `pushBackRoute`
          *
-         * 9. Trailing slashes (done)
+         * 9. Trailing slashes
          *  Some clients might not be configured to support `#/accounts/` and `#/accounts`.
          *      - Add flags to always strip trailing slashes in the `init`. Subsequently
          *      also include flag to append slash. `init({ appendSlash: true, stripSlash: true })`.
